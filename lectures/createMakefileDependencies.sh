85e187d26bb0b0291dece9ad73db2d688efbb4ef
Patrick Lam <prof.lam@gmail.com>
2020-09-28 12:23:00 +1300

    Revert "Revert "run bibtex on the lecture notes as well""
    This reverts commit 0187b273fc50943489fbb921217e17984969ebb9.

---
+++ lectures/createMakefileDependencies.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+rm -rf ./makefileDependencies
+mkdir makefileDependencies
+
+for lectureNumber in $(ls *[0-9].tex | sed -e 's/\..*//')
+do
+        lectureNote=( "$lectureNumber-$(grep -ohP \
+                      '(\\lecture.*\s?---\s?)\K([A-Za-z0-9\-\\\&,;:\/ ]*[A-Za-z0-9])' $lectureNumber.tex \
+                      | sed -e 's@/@ @g' -e 's/[^a-zA-Z0-9 -]//g' -e 's/  / /g' -e 's/ /_/g')" )
+        echo "$lectureNote.pdf: $lectureNumber.tex
+	rm -f \$@
+	pdflatex -jobname=$lectureNote \$<
+	bibtex $lectureNote
+	pdflatex -jobname=$lectureNote \$<
+	pdflatex -jobname=$lectureNote \$<" > ./makefileDependencies/$lectureNote.d
+done
+
+for slideNumber in $(ls *-slides.tex | sed -e 's/-.*//')
+do
+        slide=( "$slideNumber-slides-$(grep -ohP \
+                '(\\title.*\s?---\s?)\K([A-Za-z0-9\-\\\&,;:\/ ]*[A-Za-z0-9])' $slideNumber-slides.tex \
+                | sed -e 's@/@ @g' -e 's/[^a-zA-Z0-9 -]//g' -e 's/  / /g' -e 's/ /_/g')" )
+        echo "$slide.pdf: $slideNumber-slides.tex
+	rm -f \$@
+	pdflatex -jobname=$slide \$<
+	pdflatex -jobname=$slide \$<" > ./makefileDependencies/$slide.d
+done
