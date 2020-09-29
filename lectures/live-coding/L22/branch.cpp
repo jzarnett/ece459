65367bc18559b2aed709356e2a599d442cadbe48
Jeff Zarnett <jzarnett@gmail.com>
2020-09-29 18:33:56 -0400

    Remove old C/C++ examples from live-coding

--- lectures/live-coding/L22/branch.cpp
+++
@@ -1,9 +0,0 @@
-kernel void contains_branch(global float *a, 
-                            global float *b) {
-    int id = get_global_id(0);
-    if (cond) {
-        x[id] += 5.0;
-    } else {
-        y[id] += 5.0;
-    }
-}
