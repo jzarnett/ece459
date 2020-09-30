abeb63eb748d8c4b2a1ec6a88776fbe3f0b39cf0
Jeff Zarnett <jzarnett@gmail.com>
2020-09-30 16:55:43 -0400

    Remove C and CPP live coding examples

--- lectures/live-coding/L11-sum.c
+++
@@ -1,7 +0,0 @@
-double sum(double *array, int length)
-{
-    double total = 0;
-    for (int i = 0; i < length; i++)
-        total += array[i];
-    return total;
-}
--- lectures/live-coding/L12-parallel-for.c
+++
@@ -1,6 +0,0 @@
-  void calc (double *array1, double *array2, int length) {
-    #pragma omp parallel for
-    for (int i = 0; i < length; i++) {
-      array1[i] += array2[i];
-    }
-  }
