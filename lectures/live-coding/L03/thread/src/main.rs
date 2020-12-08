07fec7bb617712f71b519118a3886d23723b0d25
Jeff Zarnett <jzarnett@gmail.com>
2020-12-08 10:42:36 -0500

    Recorded L1/2/3

---
+++ lectures/live-coding/L03/thread/src/main.rs
@@ -0,0 +1,20 @@
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {} from the spawned thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("hi number {} from the main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    println!("Main thread has finished, and will wait for the spawned thread.");
+    handle.join().unwrap();
+    println!("Main thread has finished, and so has the thread.");
+}
