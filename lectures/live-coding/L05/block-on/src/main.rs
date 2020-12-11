202689bea675f809231104eae389fa4488192747
Jeff Zarnett <jzarnett@gmail.com>
2020-12-11 09:25:18 -0500

    Expand L05 example slightly

--- lectures/live-coding/L05/block-on/src/main.rs
+++ lectures/live-coding/L05/block-on/src/main.rs
@@ -1,15 +1,24 @@
 use futures::executor::block_on;
+use std::thread::sleep;
+use std::time::Duration;
 
-async fn q() -> u32 {
-  return 42;
+async fn q() {
+  sleep(Duration::from_secs(1));
+  println!("World.");
 }
 
-async fn hello_world() {
-  q().await;
+async fn r() {
+  sleep(Duration::from_secs(1));
   println!("hello");
 }
 
-fn main() {
+async fn hello_world() {
+  r().await;
+  q().await;
+}
+
+#[tokio::main]
+async fn main() {
   let future = hello_world();
   block_on(future);
 }
