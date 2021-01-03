b2bf6ee842f51018d98f0aeeb94c504410e350ca
Jeff Zarnett <jzarnett@gmail.com>
2021-01-03 09:54:48 -0500

    Working Helgrind example

--- lectures/live-coding/L09/helgrind/src/main.rs
+++ lectures/live-coding/L09/helgrind/src/main.rs
@@ -1,21 +1,29 @@
 use std::thread;
 use std::sync::{Mutex, Arc};
+use std::time::Duration;
 
 fn main() {
     let a = Arc::new(Mutex::new(0));
     let b = Arc::new(Mutex::new(0));
     let a2 = a.clone();
     let b2 = b.clone();
+    let a3 = a.clone();
+    let b3 = b.clone();
 
     let handle = thread::spawn(move|| {
-        *a.lock().unwrap() = 1;
-        *b.lock().unwrap() = 1;
+        let mut a = a3.lock().unwrap();
+        let mut b = b3.lock().unwrap();
+        *a = 2;
+        *b = 2;
     });
     let handle2 = thread::spawn(move|| {
-        *b2.lock().unwrap() = 1;
-        *a2.lock().unwrap() = 1;
+        let mut b = b2.lock().unwrap();
+        let mut a = a2.lock().unwrap();
+        *b = 1;
+        *a = 1;
     });
 
     handle.join().unwrap();
     handle2.join().unwrap();
+    println!{"Final Values: {}, {}", *a.lock().unwrap(), *b.lock().unwrap() }
 }
