a7110254a404cc6b4d10ef8c03da7415bce1e916
Jeff Zarnett <jzarnett@gmail.com>
2021-01-03 09:16:38 -0500

    L09 improvement

---
+++ lectures/live-coding/L09/valgrind/src/main.rs
@@ -0,0 +1,23 @@
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+
+    let handle = thread::spawn(|| {
+        let v = 42;
+        for i in 1..10 {
+            println!("hi number {} from the spawned thread!", i);
+            thread::sleep(Duration::from_millis(1));
+            std::mem::forget(v)
+        }
+    });
+
+    for i in 1..5 {
+        println!("hi number {} from the main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    println!("Main thread has finished, and will wait for the spawned thread.");
+    handle.join().unwrap();
+    println!("Main thread has finished, and so has the thread.");
+}
