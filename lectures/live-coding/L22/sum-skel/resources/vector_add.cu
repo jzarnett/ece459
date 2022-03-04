55da92ca1ce49408a66ae34d44e17b3477aca0a9
Patrick Lam <patrick.lam@uwaterloo.ca>
2022-03-03 22:40:36 -0500

    live coding

---
+++ lectures/live-coding/L22/sum-skel/resources/vector_add.cu
@@ -0,0 +1,1 @@
+// put a kernel here!
--- lectures/live-coding/L22/sum/resources/vector_add.cu
+++ lectures/live-coding/L22/sum/resources/vector_add.cu
@@ -1,5 +1,5 @@
-__global__ void vector_add(float* A, float* B, float* C)
+extern "C" __global__ void vector_add(float* A, float* B, float* C)
 {
-  int i = threadIdx.x;
+  int i = blockIdx.x;
   C[i] = A[i] + B[i];
 }
