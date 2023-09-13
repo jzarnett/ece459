9e517e8e20e5014849f349bb963e75693b2c3c52
Jeff Zarnett <jzarnett@gmail.com>
2023-09-13 16:49:49 -0400

    Update dummy_data

--- lectures/live-coding/L24/dummy_data.py
+++ lectures/live-coding/L24/dummy_data.py
@@ -5,27 +5,13 @@ import torch
 from transformers import AutoModelForSequenceClassification
 from transformers import TrainingArguments, Trainer, logging
 
-print_gpu_utilization()
-torch.ones((1, 1)).to("cuda")
-print_gpu_utilization()
-
-model = AutoModelForSequenceClassification.from_pretrained("bert-large-uncased").to("cuda")
-print_gpu_utilization()
-
-logging.set_verbosity_error()
-
-training_args = TrainingArguments(per_device_train_batch_size=4, **default_args)
-trainer = Trainer(model=model, args=training_args, train_dataset=ds)
-result = trainer.train()
-print_summary(result)
-
-seq_len, dataset_size = 512, 512
-dummy_data = {
-    "input_ids": np.random.randint(100, 30000, (dataset_size, seq_len)),
-    "labels": np.random.randint(0, 1, (dataset_size)),
+default_args = {
+    "output_dir": "tmp",
+    "evaluation_strategy": "steps",
+    "num_train_epochs": 1,
+    "log_level": "error",
+    "report_to": "none",
 }
-ds = Dataset.from_dict(dummy_data)
-ds.set_format("pt")
 
 def print_gpu_utilization():
     nvmlInit()
@@ -38,3 +24,28 @@ def print_summary(result):
     print(f"Time: {result.metrics['train_runtime']:.2f}")
     print(f"Samples/second: {result.metrics['train_samples_per_second']:.2f}")
     print_gpu_utilization()
+
+print("Starting up. Initial GPU utilization:")
+print_gpu_utilization()
+torch.ones((1, 1)).to("cuda")
+print("Initialized Torch; current GPU utilization:")
+print_gpu_utilization()
+
+model = AutoModelForSequenceClassification.from_pretrained("bert-large-uncased").to("cuda")
+print_gpu_utilization()
+
+logging.set_verbosity_error()
+
+seq_len, dataset_size = 512, 512
+dummy_data = {
+    "input_ids": np.random.randint(100, 30000, (dataset_size, seq_len)),
+    "labels": np.random.randint(0, 1, (dataset_size)),
+}
+ds = Dataset.from_dict(dummy_data)
+ds.set_format("pt")
+
+training_args = TrainingArguments(per_device_train_batch_size=1, **default_args)
+trainer = Trainer(model=model, args=training_args, train_dataset=ds)
+result = trainer.train()
+print_summary(result)
+
