6747331b992f1ae4e391cff50ca0d9c0cb827af5
Jeff Zarnett <jzarnett@gmail.com>
2023-09-16 16:01:01 -0400

    Formatting improvements for dummy_data

--- lectures/live-coding/L24/dummy_data.py
+++ lectures/live-coding/L24/dummy_data.py
@@ -1,7 +1,7 @@
 import numpy as np
+import torch
 from datasets import Dataset
 from pynvml import *
-import torch
 from transformers import AutoModelForSequenceClassification
 from transformers import TrainingArguments, Trainer, logging
 
@@ -13,17 +13,19 @@ default_args = {
     "report_to": "none",
 }
 
+
 def print_gpu_utilization():
     nvmlInit()
     handle = nvmlDeviceGetHandleByIndex(0)
     info = nvmlDeviceGetMemoryInfo(handle)
-    print(f"GPU memory occupied: {info.used//1024**2} MB.")
+    print(f"GPU memory occupied: {info.used // 1024 ** 2} MB.")
 
 
-def print_summary(result):
-    print(f"Time: {result.metrics['train_runtime']:.2f}")
-    print(f"Samples/second: {result.metrics['train_samples_per_second']:.2f}")
+def print_summary(res):
+    print(f"Time: {res.metrics['train_runtime']:.2f}")
+    print(f"Samples/second: {res.metrics['train_samples_per_second']:.2f}")
     print_gpu_utilization()
+
 
 print("Starting up. Initial GPU utilization:")
 print_gpu_utilization()
@@ -39,7 +41,7 @@ logging.set_verbosity_error()
 seq_len, dataset_size = 512, 512
 dummy_data = {
     "input_ids": np.random.randint(100, 30000, (dataset_size, seq_len)),
-    "labels": np.random.randint(0, 1, (dataset_size)),
+    "labels": np.random.randint(0, 1, dataset_size),
 }
 ds = Dataset.from_dict(dummy_data)
 ds.set_format("pt")
@@ -48,4 +50,3 @@ training_args = TrainingArguments(per_device_train_batch_size=8, **default_args)
 trainer = Trainer(model=model, args=training_args, train_dataset=ds)
 result = trainer.train()
 print_summary(result)
-
